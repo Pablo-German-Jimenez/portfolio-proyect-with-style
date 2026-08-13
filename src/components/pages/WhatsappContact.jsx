82e56a9b0e233ae390baeee51400cb2a6e55cd08
dev.break.Boy.Linkin(Style:🤸‍♂️,🧑‍💻,🎶) <pablogermanjimenez7@gmail.com>
2026-08-13 15:08:25 -0300

    Update WhatsappContact.jsx

--- src/components/pages/WhatsappContact.jsx
+++ src/components/pages/WhatsappContact.jsx
@@ -33,7 +33,7 @@ const BannerContactoWppGmail = () => {
             <svg className="icon-svg" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.272H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L12 9.545l8.073-6.052C21.69 2.28 24 3.434 24 5.457z"/>
             </svg>
-            <span>Enviar Email</span>
+            <span>Send Email</span>
           </a>
         </div>
       </div>
