842f8cb4d3a1fef9d4f06016bed850c9f0c82e6b
dev.break.Boy.Linkin(Style:🤸‍♂️,🧑‍💻,🎶) <196859099+Pablo-German-Jimenez@users.noreply.github.com>
2026-08-25 04:06:31 -0300

    add setting to run in smarthphone by app

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min.js'
 /* Estilos de Tailwind */
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import {registerSW} from 'virtual:pwa-register';
+
+registerSW({immediate:true})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
